4a703de32033c1b3740e0f10a4196eecfba51cac
Prost0-Vova <122817734+Prost0-Vova@users.noreply.github.com>
2023-06-10 17:38:00 +0200

    ff

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
-
+import Notiflix from 'notiflix';
 
 const refs = {
   dateTimePicker: document.getElementById('datetime-picker'),
@@ -10,6 +10,71 @@ const refs = {
   minutes: document.querySelector('[data-minutes]'),
   seconds: document.querySelector('[data-seconds]'),
 };
+Notiflix.Report.info('Reminder', 'Please select a date and press Start', 'Okay');
+
+let targetDate = null;
+let countInterval = null;
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+
+
+const startTimer = () => {
+  const currentDate = new Date();
+  const remainingTime = targetDate - currentDate;
+
+  if (remainingTime < 0) {
+    clearInterval(countInterval);
+    refs.startBtn.disabled = true;
+    refs.dateTimePicker.disabled = false;
+
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+
+ }
+  const time = convertMs(remainingTime);
+  const { days, hours, minutes, seconds } = time;
+
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+const startCount = () => {
+  targetDate = new Date(refs.dateTimePicker.value);
+  countInterval = setInterval(startTimer, 1000)
+  refs.startBtn.disabled = true;
+  refs.dateTimePicker.disabled = true;
+
+}
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+
+
 
 flatpickr('#datetime-picker', {
   enableTime: true,
@@ -20,3 +85,6 @@ flatpickr('#datetime-picker', {
     targetDate = selectedDates[0];
   },
 });
+
+
+refs.startBtn.addEventListener('click', startCount);
