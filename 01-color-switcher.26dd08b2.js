9cbbd7dfcace0c4c3a5a3ecfaf744516823a6945
Prost0-Vova <122817734+Prost0-Vova@users.noreply.github.com>
2023-06-13 15:37:08 +0000

    Deploying to gh-pages from @ Prost0-Vova/goit-js-hw-09@4b25f0c3f4772e3646895a07f691431a1d9f683e 🚀

---
+++ 01-color-switcher.26dd08b2.js
@@ -0,0 +1,2 @@
+const t={btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStop.disabled=!0;let e=null;function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.btnStart.addEventListener("click",(()=>{e=setInterval(a,1e3),t.btnStart.disabled=!0,t.btnStop.disabled=!1})),t.btnStop.addEventListener("click",(()=>{clearInterval(e),t.btnStart.disabled=!1,t.btnStop.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.26dd08b2.js.map
